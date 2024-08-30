8cef0dfa36e17a2782e20edf71472d3751cd331b
kkb <0furiousblade0@gmail.com>
2024-08-30 13:38:31 +0900

    [#02] Java Basic - Variables (1)

--- src/chap01/Sample.java
+++ src/chap01/Sample.java
@@ -3,9 +3,18 @@ package chap01;
 public class Sample {
     public static void main(String[] args) {
         System.out.println("hello world!");
-        // 한 줄 주석과
-        /*
-        * 여러 줄 주석
-        * */
     }
+
+    public int getResult() {
+        int result = 11;
+        return result;
+    }
+
+    public int getCnt() {
+        int cnt = 20;
+//        return result + cnt;
+//        위 주석 코드가 에러나는 이유 => 전역변수를 가져와서 사용하려 했기 때문
+        return cnt;
+    }
+
 }
