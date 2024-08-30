fe8fa0705b82f5c6215818db18f117f49c9de733
kkb <0furiousblade0@gmail.com>
2024-08-30 15:20:51 +0900

    [#04] Java Basic - Variables (3)

---
+++ src/chap02/VariableExample.java
@@ -0,0 +1,15 @@
+package chap02;
+
+public class VariableExample {
+    public static void main(String[] args) {
+        long var1 = 10;
+        long var2 = 20;
+        long var3 = 1000000000;
+        long var4 = 1000000000000L;
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+    }
+}
