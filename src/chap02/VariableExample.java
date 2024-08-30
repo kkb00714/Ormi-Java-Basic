9af5e1258b6539150a228aa34800fcc71ff51ead
kkb <0furiousblade0@gmail.com>
2024-08-30 15:48:03 +0900

    [#06] Java Basic - Variables (5)

--- src/chap02/VariableExample.java
+++ src/chap02/VariableExample.java
@@ -25,7 +25,12 @@ public class VariableExample {
         System.out.println(doubleValue2);
 
 
-
+        boolean stop = false;
+        if (stop) {
+            System.out.println("중지합니다!");
+        } else {
+            System.out.println("시작합니다!");
+        }
 
     }
 }
