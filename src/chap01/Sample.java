4a4c6a50cbc3bf955b168ee5969c1102ff08a26e
kkb <0furiousblade0@gmail.com>
2024-08-30 12:16:59 +0900

    [#01] Java Basic - - main 메서드 & 주석

---
+++ src/chap01/Sample.java
@@ -0,0 +1,11 @@
+package chap01;
+
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        // 한 줄 주석과
+        /*
+        * 여러 줄 주석
+        * */
+    }
+}
