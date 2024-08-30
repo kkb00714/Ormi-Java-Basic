a79a696b0adaade76cd2884fa258922ff006fca5
kkb <0furiousblade0@gmail.com>
2024-08-30 15:42:40 +0900

    [#05] Java Basic - Variables (4)

--- src/chap02/VariableExample.java
+++ src/chap02/VariableExample.java
@@ -11,5 +11,21 @@ public class VariableExample {
         System.out.println(var2);
         System.out.println(var3);
         System.out.println(var4);
+
+        double floatValue = 1.01234;
+        float doubleValue = 1.1234f;
+
+        System.out.println(floatValue);
+        System.out.println(doubleValue);
+
+        float floatValue2 = 0.1234567891234567f;
+        double doubleValue2 = 0.12345678912367F;
+
+        System.out.println(floatValue2);
+        System.out.println(doubleValue2);
+
+
+
+
     }
 }
