f3f727007171b73e094c05d74d006ed5a37f8ecc
kkb <0furiousblade0@gmail.com>
2024-09-02 14:15:08 +0900

    [#12] Java Basic - UnBoxing

---
+++ src/chap02/casting/Unboxing.java
@@ -0,0 +1,12 @@
+package chap02.casting;
+
+public class Unboxing {
+    public static void main(String[] args) {
+        int index = 20;
+        Integer intObject = index; // 오토박싱
+
+        int index2 = intObject; // 언박싱 ( Integer -> int )
+
+
+    }
+}
