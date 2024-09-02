a93aa9c0937df12e221dc86f6284e780db600c17
kkb <0furiousblade0@gmail.com>
2024-09-02 11:42:22 +0900

    [#09] Java Basic - Reference Type (String)

---
+++ src/chap02/Sample.java
@@ -0,0 +1,14 @@
+package chap02;
+
+public class Sample {
+    public static void main(String[] args) {
+        // String, 참조 자료형 : new 예약어를 사용해서 변수를 초기화.
+        // String은 예외
+        String drink1 = "Mountain Dew"; // 리터럴 값으로 초기화
+        String drink2 = new String("Pepsi Zero - Lime Flavor");
+        String drink3 = ""; // null => 자칫하면 NullPointException(NPE) 이 발생 => 어디에서 오류가 발생했는지 알 수가 없음 ㄷㄷㄷ
+        // 따라서 문자열을 초기화 할 때 리터럴로 null 로 초기화 하면 안 됨...
+
+        // 그러나 String은 참조 자료형임에도 불구하고 new 예약어를 사용하지 않아도 됨.
+    }
+}
--- src/chap02/casting/ForceCasting.java
+++ src/chap02/casting/ForceCasting.java
@@ -13,9 +13,21 @@ public class ForceCasting {
         // 소수를 정수로 강제 변환할 경우, 소수점 이하 부분은 버려지고 정수 부분만 저장
 
         int intVar2 = 10;
+        char charValue = 'A';
         double doubleValue = 5.5;
+        String stringValue = "A";
 
         int result = intVar2 + (int) doubleValue;
         // 정수로만 값을 넣고 싶을 때 강제 형 변환 사용
+
+        // int -> double
+        double doubleValue2 = (double) intValue;
+
+        // String -> char (캐스팅으로 변환은 x)
+//        char var = (char) stringValue;
+
+        // 가능하게 하려면 charAt(index) 라는 메서드를 사용해야 함.
+        char changedValue = stringValue.charAt(0);
+        System.out.println("changedValue = " + changedValue);
     }
 }
