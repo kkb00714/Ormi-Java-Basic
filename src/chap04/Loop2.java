fe6b2fcb3ce1cdf85085b6c6b3eba21be85e961f
kkb <0furiousblade0@gmail.com>
2024-09-04 13:27:43 +0900

    [#27] Java Basic - Loop (2)

---
+++ src/chap04/Loop2.java
@@ -0,0 +1,9 @@
+package chap04;
+
+public class Loop2 {
+    public static void main(String[] args) {
+        for (int index = 0; index < args.length; index++) {
+            System.out.println(args[index]);
+        }
+    }
+}
