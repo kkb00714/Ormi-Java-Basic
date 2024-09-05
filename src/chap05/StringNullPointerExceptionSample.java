9a5045fe9237e5c52635b459bbf66e05aabba5d0
kkb <0furiousblade0@gmail.com>
2024-09-05 11:13:51 +0900

    [#34] Java Basic - NPE Test

---
+++ src/chap05/StringNullPointerExceptionSample.java
@@ -0,0 +1,10 @@
+package chap05;
+
+public class StringNullPointerExceptionSample {
+    public static void main(String[] args) {
+//        String str = null;
+//        System.out.println(str.length());
+        // 오류가 날 건데 빨간줄이 안 보여... ㄸㄸㄸㄸ...
+//        System.out.println(str.equals("비교하려는 값"));
+    }
+}
