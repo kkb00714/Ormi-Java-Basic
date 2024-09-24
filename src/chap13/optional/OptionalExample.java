756200be14ee2e70d8a36d1b4079d8cf16552b1e
kkb <0furiousblade0@gmail.com>
2024-09-24 15:02:56 +0900

    [#109] Java Basic - Optional (2)

--- src/chap13/optional/OptionalExample.java
+++ src/chap13/optional/OptionalExample.java
@@ -16,5 +16,36 @@ public class OptionalExample {
         Optional.ofNullable(list)
                 .orElseGet(Collections::emptyList)
                 .forEach(System.out::println);
+
+        // Optional 객체 생성 (empty(), of(), ofNullable())
+        Optional<Integer> empty = Optional.empty();  // 비어 있는 optional 객체 생성
+        Optional<Integer> optional2 = Optional.of(456);  // 절대 null이 아닌 Optional 객체 생성
+        // null 값을 허용하지 않기 때문에, null 을 넣으려고 하면 즉시 에러 발생
+
+        Optional<Integer> optional3 = Optional.ofNullable(2324);  // 값이 있을 수도 있고 없을 수도 있는 Optional 객체 생성
+        Optional<String> optional4 = Optional.of("231");
+
+        empty.ifPresent(System.out::println);  //
+        optional2.ifPresent(System.out::println);  // 456
+        optional3.ifPresent(System.out::println);  // 2324
+        optional4.ifPresent(System.out::println);  // 231
+
+
+        // Optiional 객체 꺼내기 (반환)
+        // 1. T get()
+
+
+        // 2. T orElse(T other)
+
+
+        // 3. T orElseGet(Supplier<? extends T> other)
+
+
+        // 4.
+
+
+        // 5. boolean isPresent()
+
+
     }
 }
