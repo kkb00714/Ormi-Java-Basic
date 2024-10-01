72b842967257f5291c563478bc31c741debc0b2b
kkb <0furiousblade0@gmail.com>
2024-10-01 19:26:54 +0900

    [#121] Java Review - 자바와 친해지길 바라 2

---
+++ src/JavaReviewExample/ReviewProject_2.java
@@ -0,0 +1,24 @@
+package JavaReviewExample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReviewProject_2 {
+    public static void main(String[] args) {
+        // ArrayList
+
+        List<String> iam = new ArrayList<>(); // 기본적으로 크기가 10인 배열 생성
+        iam.add("요소1");
+        iam.add("요소3");
+        iam.add("요소2");
+        iam.add("요소4");
+        iam.add("요소1");
+
+        System.out.println(iam);
+        // ArrayList는 그냥 print해도 나온다.
+        // 순서가 있고 중복제거는 되지 않는다.
+
+
+
+    }
+}
